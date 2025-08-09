d3088e9fb7b6ff5dabd4d7ce98a64a41663a90ff
aburossi <142565006+aburossi@users.noreply.github.com>
2025-08-09 14:27:02 +0200

    Update list-chapters.js

--- netlify/functions/list-chapters.js
+++ netlify/functions/list-chapters.js
@@ -1,13 +1,25 @@
+// netlify/functions/list-chapters.js
+
 const AWS = require('aws-sdk');
 
 exports.handler = async (event) => {
   try {
-    const s3 = new AWS.S3({
-      region: process.env.AWS_REGION,
-    });
+    // BEFORE: This uses the reserved (and therefore undefined) variable
+    // const s3 = new AWS.S3({
+    //   region: process.env.AWS_REGION,
+    // });
+    // const bucket = process.env.AWS_S3_BUCKET_NAME;
 
-    const bucket = process.env.AWS_S3_BUCKET_NAME;
-    const prefix = process.env.AWS_S3_PREFIX || '';
+    // AFTER: Explicitly use your custom variables
+    const s3 = new AWS.S3({
+      region: process.env.APP_AWS_REGION,
+      accessKeyId: process.env.APP_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.APP_AWS_SECRET_ACCESS_KEY,
+    });
+    const bucket = process.env.APP_AWS_S3_BUCKET_NAME;
+
+
+    const prefix = process.env.AWS_S3_PREFIX || ''; // This one isn't reserved
     const params = {
       Bucket: bucket,
       Prefix: prefix,
