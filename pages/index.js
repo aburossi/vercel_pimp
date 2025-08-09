22f3f72e5c42bf30491248bf8b4c5bf160d87e29
aburossi <142565006+aburossi@users.noreply.github.com>
2025-08-09 04:19:38 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif' }}>
+      <h1>Willkommen zu PimpMyTextbook für Vercel</h1>
+      <p>Diese Seite dient als Platzhalter für die künftige Anwendung.</p>
+    </div>
+  );
+}
